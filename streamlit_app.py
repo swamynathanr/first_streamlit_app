50677c6abe7aa91522834de89b7891e9704a7f91
swamynathanr <67514566+swamynathanr@users.noreply.github.com>
2022-04-26 23:18:56 +0530

    Update streamlit_app.py
    Show fruit name on multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index("Fruit")
 # put picklist here
 streamlit.multiselect("Put some fruits:",list(my_fruit_list.index))
 # Display the table on the page
