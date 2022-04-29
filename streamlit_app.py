c6a72f83c59ee8f50ea7e95cb2a018ba3338af3d
swamynathanr <67514566+swamynathanr@users.noreply.github.com>
2022-04-29 17:11:35 +0530

    Update streamlit_app.py
    #New section to display fruityvice api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
-#streamlit.title('My Parents New Healthy Diner')
-streamlit.title('Aruna\'s kithcen')
+streamlit.title('My Parents New Healthy Diner')
+#streamlit.title('Aruna\'s kithcen')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
@@ -15,3 +15,7 @@ fruits_selected=streamlit.multiselect("Put some fruits:",list(my_fruit_list.inde
 fruits_to_show=my_fruit_list.loc[fruits_selected]
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
+#New section to display fruityvice api response
+import requests
+fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response
